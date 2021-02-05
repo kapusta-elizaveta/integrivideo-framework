8c8d22062c35456553559b71a9189f7c256ddd2c
Lizaveta Kapusta <kapustaeliz@gmail.com>
2021-02-05 15:15:26 +0300

    add user modal

---
+++ IntegriVideoProject/Services/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using WebCore;
+
+namespace Services.Models
+{
+    public class User
+    {
+        public string Email { get; }
+        public string Password { get; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public static User WithCredentialsFromProperty()
+        {
+            return new User(Configurator.Email, Configurator.Password);
+        }
+    }
+}
