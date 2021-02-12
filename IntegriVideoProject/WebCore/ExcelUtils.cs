199e72b107c3b3cf6b703cbb1916ef109939985e
Lizaveta Kapusta <kapustaeliz@gmail.com>
2021-02-12 17:29:07 +0300

    add excelUtils

---
+++ IntegriVideoProject/WebCore/ExcelUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using ExcelDataReader;
+using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace WebCore
+{
+    public class ExcelUtils
+    {
+        private static string pathToFile = @"C:\Users\alexey\data.xlsx";
+    }
+}
+        
+       
+
+
+ 
